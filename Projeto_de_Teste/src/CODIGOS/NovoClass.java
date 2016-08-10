a1bceb455c5f1ec4788724b5fd1e1023be54d39c
Ismael Ribeiro <Ismael Ribeiro@DESKTOP-87C84IR>
2016-08-10 15:00:13 -0300

    * Primeiras alteracoes

---
+++ Projeto_de_Teste/src/CODIGOS/NovoClass.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CODIGOS;
+
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Ismael Ribeiro
+ */
+public class NovoClass {
+    public static void main(String args[]){
+        
+        double x = 3.14;
+        double y = 2.452134;
+        double resultado = (x*y);
+        
+        DecimalFormat deci = new DecimalFormat("0.0");
+     
+         JOptionPane.showMessageDialog(null,deci.format(resultado));
+        
+    }
+}
